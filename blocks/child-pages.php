dfbd82fb0c82380c3513b04e3e3f190c012dc202
Ned Zimmerman <ned@bight.ca>
2019-05-10 11:07:58 -0300

    Add fields to plugin (#6)

--- blocks/child-pages.php
+++ blocks/child-pages.php
@@ -1,10 +1,84 @@
 <?php
 
-acf_register_block_type([
+acf_register_block_type(array(
   'name' => 'child-pages',
   'title' => __('Child Pages'),
   'render_template' => __DIR__ . '/templates/child-pages.php',
-  'supports' => [
+  'supports' => array(
     'align' => false,
-  ]
-]);
+  )
+));
+
+acf_add_local_field_group(array(
+    'key' => 'group_5cd425a9e3cc7',
+    'title' => 'Child Pages',
+    'fields' => array(
+        array(
+            'key' => 'field_5cd425c8611c5',
+            'label' => 'Parent Page',
+            'name' => 'parent',
+            'type' => 'post_object',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'post_type' => array(
+                0 => 'page',
+            ),
+            'taxonomy' => '',
+            'allow_null' => 1,
+            'multiple' => 0,
+            'return_format' => 'id',
+            'ui' => 1,
+        ),
+        array(
+            'key' => 'field_5cd429c65f675',
+            'label' => 'Style',
+            'name' => 'style',
+            'type' => 'select',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'choices' => array(
+                'child-pages--card' => 'Card',
+                'child-pages--card-with-excerpt' => 'Card with Excerpt',
+                'child-pages--text-only' => 'Text Only',
+            ),
+            'default_value' => array(
+                0 => 'child-pages--card',
+            ),
+            'allow_null' => 0,
+            'multiple' => 0,
+            'ui' => 0,
+            'return_format' => 'value',
+            'ajax' => 0,
+            'placeholder' => '',
+        ),
+    ),
+    'location' => array(
+        array(
+            array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/child-pages',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+));
--- blocks/social-links.php
+++ blocks/social-links.php
@@ -1,10 +1,99 @@
 <?php
 
-acf_register_block_type([
+acf_register_block_type(array(
   'name' => 'social-links',
   'title' => __('Social Links'),
   'render_template' => __DIR__ . '/templates/social-links.php',
-  'supports' => [
+  'supports' => array(
     'align' => false,
-  ]
-]);
+  )
+));
+
+acf_add_local_field_group(array(
+    'key' => 'group_5cd580fbefcbd',
+    'title' => 'Social Links',
+    'fields' => array(
+        array(
+            'key' => 'field_5cd58100e5b2b',
+            'label' => 'Style',
+            'name' => 'style',
+            'type' => 'select',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'choices' => array(
+                'default' => 'Default',
+                'icon-only' => 'Icon Only',
+            ),
+            'default_value' => array(
+                0 => 'default',
+            ),
+            'allow_null' => 0,
+            'multiple' => 0,
+            'ui' => 0,
+            'return_format' => 'value',
+            'ajax' => 0,
+            'placeholder' => '',
+        ),
+        array(
+            'key' => 'field_5cd582a254dc7',
+            'label' => 'Facebook Label',
+            'name' => 'label_facebook',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => 'Platform Cooperativism – Discussion & Linkshare',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+        array(
+            'key' => 'field_5cd582e554dc8',
+            'label' => 'Twitter Label',
+            'name' => 'label_twitter',
+            'type' => 'text',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'default_value' => 'Platform Co-op Development Kit',
+            'placeholder' => '',
+            'prepend' => '',
+            'append' => '',
+            'maxlength' => '',
+        ),
+    ),
+    'location' => array(
+        array(
+            array(
+                'param' => 'block',
+                'operator' => '==',
+                'value' => 'acf/social-links',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+));
