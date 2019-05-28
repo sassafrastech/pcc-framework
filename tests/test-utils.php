a81f2f0ac0c0e4eb37d40bfe98d644a5a0f97806
Ned Zimmerman <ned@bight.ca>
2019-05-28 11:09:57 -0300

    Fix tests?

---
+++ tests/test-utils.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Class UtilsTest
+ *
+ * @package PCCFramework
+ */
+
+use function PCCFramework\Utils\get_config_option;
+
+/**
+ * Utils tests.
+ */
+class UtilsTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_get_config_option() {
+        update_option('platformcoop_configuration', ['my_key' => 'my_value']);
+        $result = get_config_option('my_key', ['my_key' => 'my_default_value']);
+        $this->assertEquals('my_value', $result);
+        delete_option('platformcoop_configuration');
+        $result = get_config_option('my_other_key', ['my_other_key' => 'my_other_default_value']);
+		$this->assertEquals('my_other_default_value', $result);
+	}
+}
